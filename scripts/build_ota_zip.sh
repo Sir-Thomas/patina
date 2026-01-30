58f80b1346ab2c2511886c55c8c8b8cbbe7af552
Sir-Thomas <tp8153@gmail.com>
2026-01-30 12:29:51 -0600

    disable logging when building ota image

--- scripts/build_ota_zip.sh
+++ scripts/build_ota_zip.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-cargo objcopy --release -- -O binary ./images/patina.bin
+DEFMT_LOG=off cargo objcopy --release -- -O binary ./images/patina.bin
 scripts/imgtool.py create --header-size 512 --align 4 --version 1.0.0 --slot-size 475136 --pad-header ./images/patina.bin ./images/patina-image.bin
 adafruit-nrfutil dfu genpkg --dev-type 0x0052 --application ./images/patina-image.bin ./images/patina-dfu.zip

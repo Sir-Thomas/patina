8af4881016c6d60028c98780e4b0cb70eb68b433
Sir-Thomas <tp8153@gmail.com>
2026-01-30 14:17:44 -0600

    add script for local flash with mcuboot

---
+++ scripts/run_on_mcuboot.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+DEFMT_LOG=off cargo objcopy --release -- -O binary ./images/patina.bin
+scripts/imgtool.py create --header-size 512 --align 4 --version 1.0.0 --slot-size 475136 --pad-header ./images/patina.bin ./images/patina-image.bin
+probe-rs download --chip nrf52832_xxaa --binary-format Binary --base-address 0x8000 ./images/patina-image.bin

318419655ba3944e8cb34791d78ae5b3c1d9380c
Sir-Thomas <tp8153@gmail.com>
2026-02-01 08:25:47 -0600

    flip screen

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use defmt_rtt as _;
 use embassy_executor::Spawner;
 use embassy_nrf::interrupt::Priority;
 use panic_probe as _;
-use pinetime_bsp::PineTime;
+use pinetime_bsp::{PineTime, ScreenOrientation};
 
 mod app_framework;
 mod apps;
@@ -28,7 +28,8 @@ async fn main(spawner: Spawner) {
     config.lfclk_source = embassy_nrf::config::LfclkSource::ExternalXtal;
     config.gpiote_interrupt_priority = Priority::P2;
     config.time_interrupt_priority = Priority::P2;
-    let board = PineTime::new(config).await;
+    let mut board = PineTime::new(config).await;
+    board.set_screen_orientation(ScreenOrientation::Flipped).await;
 
     info!("Spawning systick task");
     spawner.must_spawn(tasks::systick::systick_task());

86cade7168bab7aa68fb87aa25197e316504d1db
Sir-Thomas <tp8153@gmail.com>
2026-01-31 22:00:00 -0600

    decouple from driver

--- src/tasks/touch.rs
+++ src/tasks/touch.rs
@@ -1,14 +1,11 @@
-use cst816s::CST816S; // TODO: Decouple from driver
-use embassy_embedded_hal::shared_bus::asynch::i2c::I2cDevice;
-use embassy_nrf::{gpio::{Input, Output}, twim::Twim};
-use embassy_sync::blocking_mutex::raw::NoopRawMutex;
+use pinetime_bsp::touch::TouchController;
 
 use crate::{app_framework::SystemEvent, signals::EVENT_QUEUE};
 
 #[embassy_executor::task]
-pub async fn touch_task(mut touch: CST816S<I2cDevice<'static, NoopRawMutex, Twim<'static>>, Input<'static>, Output<'static>>) {
+pub async fn touch_task(mut touch: TouchController) {
     loop {
-        let touch_event = touch.wait_for_touch().await.unwrap();
+        let touch_event = touch.wait_for_touch().await;
         EVENT_QUEUE.send(SystemEvent::Touch(touch_event)).await;
     }
 
